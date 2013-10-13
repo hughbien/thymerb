df4d7c1e132ff41003eec009f939e1fa2d6f33b8
Hugh Bien <hugh@hughbien.com>
2013-10-13 02:54:16 -0700

    Use Compass reset styles.

--- site/controller.rb
+++ site/controller.rb
@@ -1,7 +1,11 @@
 require 'rubygems'
 require 'homeostasis'
+require 'compass'
 
-Homeostasis::Asset.concat 'all.css', %w(reset.css styles.css)
+Stasis::Options.set_template_option 'scss', Compass.sass_engine_options
+Homeostasis::Asset.concat 'all.css', %w(styles.css)
 Homeostasis::Sitemap.config(url: 'http://thymerb.com')
 
 ignore /\/_.*/
+ignore /\/\.saas-cache\/.*/
+ignore /.*\.scssc/
